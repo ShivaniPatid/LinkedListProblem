df5e862f6d876db9e1ec140694dcb4166915ac48
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-13 18:35:14 +0530

    [Shivani] Uc4 - Insert an element in between linked list

--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -10,7 +10,7 @@ namespace LinkedListProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose a option\n 1. Add an elements in linked list\n");
+            Console.WriteLine("Choose a option\n 1. Add an elements in linked list\n 4. Insert an element in between linked list");
             int option=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
             switch (option)
@@ -21,6 +21,13 @@ namespace LinkedListProblem
                     list1.Add(30);
                     list1.Add(70);
                     list1.Display();
+                    break;
+                case 4:
+                    InsertElementInLinkedList list2 = new InsertElementInLinkedList();
+                    list2.Add(56);
+                    list2.Add(70);
+                    list2.InsertInBetween(30, 2);
+                    list2.Display();
                     break;
                 default:
                     Console.WriteLine("Choose a appropriate option");
