731f74bf69dea8cdfef3c03ce778a7c831260723
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-13 19:13:12 +0530

    [Shivani] UC6 - Delete last element in linked list

--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -10,7 +10,7 @@ namespace LinkedListProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose a option\n 1. Add an elements in linked list\n 4. Insert an element in between linked list\n 5. Delete a first element in linked list ");
+            Console.WriteLine("Choose a option\n 1. Add an elements in linked list\n 4. Insert an element in between linked list\n 5. Delete a first element in linked list\n 6. Delete last element in the linked list ");
             int option=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
             switch (option)
@@ -23,20 +23,29 @@ namespace LinkedListProblem
                     list1.Display();
                     break;
                 case 4:
-                    InsertElementInLinkedList list2 = new InsertElementInLinkedList();
-                    list2.Add(56);
-                    list2.Add(70);
-                    list2.InsertInBetween(30, 2);
-                    list2.Display();
+                    InsertElementInLinkedList list4 = new InsertElementInLinkedList();
+                    list4.Add(56);
+                    list4.Add(70);
+                    list4.InsertInBetween(30, 2);
+                    list4.Display();
                     break;
                 case 5:
-                    DeleteFirstElementInLinkedList list3 = new DeleteFirstElementInLinkedList();
-                    list3.Add(56);
-                    list3.Add(30);
-                    list3.Add(70);
-                    Console.WriteLine("{0} is a deleted in the linked list ",list3.DeleteFirstNode());
-                    Console.WriteLine("Element in linked list : ");
-                    list3.Display();
+                    DeleteFirstElementInLinkedList list5 = new DeleteFirstElementInLinkedList();
+                    list5.Add(56);
+                    list5.Add(30);
+                    list5.Add(70);
+                    list5.DeleteFirstNode();
+                    Console.WriteLine("After deleted the first element in linked list : ");
+                    list5.Display();
+                    break;
+                case 6:
+                    DeleteLastElementInLinkedList list6 = new DeleteLastElementInLinkedList();
+                    list6.Add(56);
+                    list6.Add(30);
+                    list6.Add(70);
+                    list6.DeleteLastNode();
+                    Console.WriteLine("Before deleted the first element in linked list : ");
+                    list6.Display();
                     break;
                 default:
                     Console.WriteLine("Choose a appropriate option");
