2c700cdc5db124823d5a9594c18ea7cce962e6ad
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-13 18:58:26 +0530

    [Shivani] UC5 - Delete first element in the linked list

--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -10,7 +10,7 @@ namespace LinkedListProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose a option\n 1. Add an elements in linked list\n 4. Insert an element in between linked list");
+            Console.WriteLine("Choose a option\n 1. Add an elements in linked list\n 4. Insert an element in between linked list\n 5. Delete a first element in linked list ");
             int option=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
             switch (option)
@@ -28,6 +28,15 @@ namespace LinkedListProblem
                     list2.Add(70);
                     list2.InsertInBetween(30, 2);
                     list2.Display();
+                    break;
+                case 5:
+                    DeleteFirstElementInLinkedList list3 = new DeleteFirstElementInLinkedList();
+                    list3.Add(56);
+                    list3.Add(30);
+                    list3.Add(70);
+                    Console.WriteLine("{0} is a deleted in the linked list ",list3.DeleteFirstNode());
+                    Console.WriteLine("Element in linked list : ");
+                    list3.Display();
                     break;
                 default:
                     Console.WriteLine("Choose a appropriate option");
